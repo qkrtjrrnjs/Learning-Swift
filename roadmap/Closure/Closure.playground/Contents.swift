fa0e214bd8a32ca4f882bfd8d6b58cfc0b0e5f97
Seok Kwun Park <park960@purdue.edu>
2019-05-16 18:25:50 -0400

    basic closure

--- roadmap/Closure/Closure.playground/Contents.swift
+++ roadmap/Closure/Closure.playground/Contents.swift
@@ -1,3 +1,30 @@
 import UIKit
 
-var str = "Hello, playground"
+/*
+ 
+ Closure: self-contained blocks of functionality that can be passed around and used in your code. Closures can capture and store references to any constants and variables from the context in which they are defined. This is known as closing over those constants and variables.
+ 
+ */
+
+//function to multiply two numbers
+func multiplyTwoNumbers(a: Int, b: Int) -> Int{
+    return a * b
+}
+
+//using closure
+let closure = { (a, b) -> Int in
+    return a * b
+}
+
+//using shorthand closure
+
+let shortClosure: (Int, Int) -> Int = {
+    return $0 * $1
+}
+
+let shorterClosure: (Int, Int) -> Int = {$0 * $1}
+
+print(multiplyTwoNumbers(a: 10, b: 10))
+print(closure(10, 10))
+print(shortClosure(10, 10))
+print(shorterClosure(10, 10))
