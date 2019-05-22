da4ceb289e4ee95a4f51d62928fa9c5c19565cff
Seok Kwun Park <park960@purdue.edu>
2019-05-22 14:27:34 -0400

    basic init example

--- roadmap/Initializers/Initializers.playground/Contents.swift
+++ roadmap/Initializers/Initializers.playground/Contents.swift
@@ -1,2 +1,19 @@
 import UIKit
 
+//basic init example
+struct student{
+    var gender: String
+    var standing: String
+    var GPA: Double
+    
+    init(gender: String, standing: String, GPA: Double){
+        self.gender = gender
+        self.standing = standing
+        self.GPA = GPA
+    }
+}
+
+let student1 = student(gender: "male", standing: "junior", GPA: 3.5)
+
+print("gender: \(student1.gender)\nstanding: \(student1.standing)\nGPA: \(student1.GPA)")
+
