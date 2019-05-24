73983a160402ca065328ad49b024f86c25bc1c5a
Seok Kwun Park <park960@purdue.edu>
2019-05-23 20:57:36 -0400

    default initializer

--- roadmap/Initializers/Initializers.playground/Contents.swift
+++ roadmap/Initializers/Initializers.playground/Contents.swift
@@ -17,3 +17,12 @@ let student1 = student(gender: "male", standing: "junior", GPA: 3.5)
 
 print("gender: \(student1.gender)\nstanding: \(student1.standing)\nGPA: \(student1.GPA)")
 
+//swift provides default initializers
+class teacher{
+    var name = "John Doe"
+    var subject = "Computer Science"
+}
+
+let teacher1 = teacher()
+
+print("\nname: \(teacher1.name)\nsubject: \(teacher1.subject)")
