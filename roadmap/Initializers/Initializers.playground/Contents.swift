b785bc41212e00cbe5e68c93b7b824d544a3b448
Seok Kwun Park <park960@purdue.edu>
2019-05-25 15:57:02 -0400

    more initializer examples

--- roadmap/Initializers/Initializers.playground/Contents.swift
+++ roadmap/Initializers/Initializers.playground/Contents.swift
@@ -1,7 +1,7 @@
 import UIKit
 
 //basic init example
-struct student{
+struct A{
     var gender: String
     var standing: String
     var GPA: Double
@@ -13,16 +13,46 @@ struct student{
     }
 }
 
-let student1 = student(gender: "male", standing: "junior", GPA: 3.5)
+let personA = A(gender: "male", standing: "junior", GPA: 3.5)
 
-print("gender: \(student1.gender)\nstanding: \(student1.standing)\nGPA: \(student1.GPA)")
+print("gender: \(personA.gender)\nstanding: \(personA.standing)\nGPA: \(personA.GPA)")
 
 //swift provides default initializers
-class teacher{
+class B{
     var name = "John Doe"
     var subject = "Computer Science"
 }
 
-let teacher1 = teacher()
+let personB = B()
 
-print("\nname: \(teacher1.name)\nsubject: \(teacher1.subject)")
+print("\nname: \(personB.name)\nsubject: \(personB.subject)")
+
+//initializer that takes no arguments
+struct C{
+    var salary: Int
+    var age: Int
+    
+    init(){
+        self.salary = 90000
+        self.age = 50
+    }
+}
+
+let personC = C()
+
+print("\nsalary: $\(personC.salary)\nage: \(personC.age)")
+
+//initializer parameters with no argument labels
+struct D{
+    var gender: String
+    var age: Int
+    
+    init(_ gender: String, _ age: Int){
+        self.gender = gender
+        self.age = age
+    }
+}
+
+let personD = D("female", 21)
+
+print("\ngender: \(personD.gender)\nage: \(personD.age)")
