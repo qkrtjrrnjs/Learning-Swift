8d20c942a85fbb1845e63bd85babd58bf8b75e51
Seok Kwun Park <park960@purdue.edu>
2019-06-10 16:50:35 -0400

    more Swift code

---
+++ roadmap/Welcome_To_Swift.playground/Contents.swift
@@ -0,0 +1,22 @@
+import UIKit
+
+//Constant Declaration * immutable *
+let hundredConst = 100
+
+//variable Declaration
+var hundredVar = 100
+var x = 1, y = 2, z = 3
+
+//type annotation
+var stringVar: String // --> declare variable called string of type String
+var a, b, c: Int // --> declare variables a, b, and c of type Int
+
+//supports string interpolation (evaluating string literals containing placeholders which replaces the placeholders with corresponding values
+print("\(hundredConst)세 시대")
+
+//typealias (defining alternative name for existing type) example
+typealias s = String
+var typealiasString: s
+
+
+
