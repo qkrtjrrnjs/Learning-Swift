5d58c2efff1ea4167c7bdd645a11ef637f86873d
Seok Kwun Park <park960@purdue.edu>
2019-06-18 22:27:34 -0400

    update on learning swift

--- roadmap/Welcome_To_Swift.playground/Contents.swift
+++ roadmap/Welcome_To_Swift.playground/Contents.swift
@@ -1,22 +1,64 @@
 import UIKit
 
-//Constant Declaration * immutable *
-let hundredConst = 100
+func TheBasics(){
+    
+// Constants and Variables
+    
+    //Constant Declaration * immutable *
+    let hundredConst = 100
 
-//variable Declaration
-var hundredVar = 100
-var x = 1, y = 2, z = 3
+    //variable Declaration
+    var hundredVar = 100
+    var x = 1, y = 2, z = 3
 
-//type annotation
-var stringVar: String // --> declare variable called string of type String
-var a, b, c: Int // --> declare variables a, b, and c of type Int
+    //type annotation
+    var stringVar: String // --> declare variable called string of type String
+    var a, b, c: Int // --> declare variables a, b, and c of type Int
 
-//supports string interpolation (evaluating string literals containing placeholders which replaces the placeholders with corresponding values
-print("\(hundredConst)세 시대")
+    //supports string interpolation (evaluating string literals containing placeholders which replaces the placeholders with corresponding values
+    print("\(hundredConst)세 시대")
 
-//typealias (defining alternative name for existing type) example
-typealias s = String
-var typealiasString: s
+// Typealias
+    
+    //typealias (defining alternative name for existing type) example
+    typealias s = String
+    var typealiasString: s
 
+// Tuples
+    
+    //tuples
+    let nameAndAge = (name: "James", age: 25)
+    print("name: \(nameAndAge.name)\nage: \(nameAndAge.age)")
+
+    //decompose tuples
+    let (name, age) = nameAndAge
+    print("name: \(name)\nage: \(age)")
+
+// Optionals
+    
+    //use optionals when values may be absent (with optionals, either there is a value or there isn't a value)
+    var year: String?
+
+    func printYear(){
+        if year != nil{
+            print(year!)
+        }
+        else{
+            print("nil")
+        }
+    }
+
+    printYear()
+
+    year = "2019"
+
+    printYear()
+    
+// Optional Binding
+    
+    
+}
+
+TheBasics()
 
 
